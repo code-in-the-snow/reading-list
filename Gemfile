7b7c89eef5a66807ae92a6458e851573f14958b5
code-in-the-snow <pscott_74@yahoo.com>
2015-08-10 12:10:07 -0400

    Added poltergeist and other test env gems.

--- Gemfile
+++ Gemfile
@@ -12,4 +12,11 @@ group :test, :development do
   gem 'byebug'
   gem 'capybara'
   gem 'rspec'
+  gem 'web-console', '~> 2.0'
+  gem 'launchy'
+  gem 'shoulda-matchers' require: false
+  gem 'poltergeist'
+  gem 'database_cleaner'
+  gem 'valid-attribute'
+  gem 'factory-girl'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,14 @@
+ENV["RAILS_ENV"] ||= "test"
+
 require 'byebug'
 require 'rspec'
 require 'capybara/rspec'
+require "capybara/poltergeist"
+require "database_cleaner"
+require "shoulda-matchers"
+require 'valid-attribute'
+require 'factory-girl'
+require 'launchy'
 
 require_relative '../server'
 
@@ -8,3 +16,38 @@ set :environment, :test
 set :database, :test
 
 Capybara.app = Sinatra::Application
+
+#################### stuff added for poltergeist & database cleaner
+#require File.expand_path("../../config/environment", __FILE__)
+
+Capybara.default_selector = :css
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app,
+                                    window_size: [1920, 1080],
+                                    phantomjs_logger: nil)
+end
+Capybara.javascript_driver = :poltergeist
+
+ActiveRecord::Migration.maintain_test_schema!
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    # set the default
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, type: :feature) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.append_after(:each) do
+    DatabaseCleaner.clean
+  end
+end
